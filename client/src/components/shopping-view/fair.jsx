3a3dc62aa315e8acb8c8ad0f05ea83e68172681d
kmoin1309 <moinoddinkazi1234@gmail.com>
2024-10-08 11:48:37 +0530

    Fixed fair.jsx

--- client/src/components/shopping-view/fair.jsx
+++ client/src/components/shopping-view/fair.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
-import "leaflet/dist/leaflet.css";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
-import "./Fair.css";
 
 const locationIcon = new L.Icon({
-  iconUrl: markerIcon, // Updated to use imported icon
+  iconUrl: markerIcon,
   iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
 });
-// Custom icon for the markers
+
 const tradeFairs = [
   {
     event: "World Food India 2024",
@@ -19,7 +17,7 @@ const tradeFairs = [
     venue: "New Delhi",
     sector: "Food processing industry",
     website: "https://worldfoodindia.gov.in/",
-    position: [28.6139, 77.209], // New Delhi coordinates
+    position: [28.6139, 77.209],
   },
   {
     event: "Uttar Pradesh International Trade Show (UPITS)",
@@ -28,7 +26,7 @@ const tradeFairs = [
     sector:
       "Manufacturing, Agriculture, Textiles, Pharmaceuticals, IT, Tourism, and more",
     website: "https://www.upinternationaltradeshow.com",
-    position: [28.4744, 77.503], // Greater Noida coordinates
+    position: [28.4744, 77.503],
   },
   {
     event: "BHARAT TELECOM 2024",
@@ -36,7 +34,7 @@ const tradeFairs = [
     venue: "Pragati Maidan, New Delhi",
     sector: "Telecom Equipment & Services",
     website: "https://www.telecomepc.in/",
-    position: [28.6205, 77.2333], // Pragati Maidan coordinates
+    position: [28.6205, 77.2333],
   },
   {
     event: "India Chem 2024",
@@ -45,26 +43,27 @@ const tradeFairs = [
     sector:
       "Chemicals, Petrochemical, Agrochemical Industry, Process and Machinery",
     website: "https://indiachem.ficci.in/",
-    position: [19.2272, 72.8561], // Mumbai coordinates
+    position: [19.2272, 72.8561],
   },
   {
     event: "18th India International Silk Fair",
     date: "7-9 November, 2024",
     venue: "The Yashobhoomi, IICC, New Delhi",
     sector: "Indian silk & silk blend products, Textiles",
-    website: "http://www.ii silkfair.com",
-    position: [28.6139, 77.209], // New Delhi coordinates
+    website: "http://www.iisilkfair.com",
+    position: [28.6139, 77.209],
   },
 ];
 
 const Fair = () => {
   return (
-    <div className="fair-container">
+    <div className="p-5 bg-gray-100 rounded-lg shadow-md">
       <h2 className="text-xl font-bold mb-4">Upcoming Trade Fairs</h2>
       <MapContainer
         center={[20.5937, 78.9629]} // Center of India
         zoom={5}
         style={{ height: "400px", width: "100%" }}
+        className="rounded-lg"
       >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
